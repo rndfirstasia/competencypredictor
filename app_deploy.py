3066eca2859406042e9e46f61de07da1ea5a7741
rndfirstasia <158240722+rndfirstasia@users.noreply.github.com>
2024-10-17 14:29:23 +0700

    Update app_deploy.py

--- app_deploy.py
+++ app_deploy.py
@@ -13,9 +13,33 @@ from collections import defaultdict
 
 #page config
 st.set_page_config(
-    page_icon="img/icon.png",
-    page_title="Prediksi Kompetensi",
+    page_icon="img/icon.png"
 )
+def set_page_title(title):
+    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
+        <iframe height=0 srcdoc="<script>
+            const title = window.parent.document.querySelector('title') \
+                
+            const oldObserver = window.parent.titleObserver
+            if (oldObserver) {{
+                oldObserver.disconnect()
+            }} \
+
+            const newObserver = new MutationObserver(function(mutations) {{
+                const target = mutations[0].target
+                if (target.text !== '{title}') {{
+                    target.text = '{title}'
+                }}
+            }}) \
+
+            newObserver.observe(title, {{ childList: true }})
+            window.parent.titleObserver = newObserver \
+
+            title.text = '{title}'
+        </script>" />
+    """)
+
+set_page_title("Prediksi Kompetensi")
 
 #env
 #taruh semua credential ke secrets
