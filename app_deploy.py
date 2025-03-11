cc79bb34492ce37836a916ce24eb73911b2bcb4e
rndfirstasia <158240722+rndfirstasia@users.noreply.github.com>
2025-03-11 15:44:39 +0700

    Update app_deploy.py

--- app_deploy.py
+++ app_deploy.py
@@ -1155,7 +1155,7 @@ with tab3:
             SELECT a.id_level_set, lvl.name_level AS 'NAMA LEVEL'
             FROM txtan_audio a
             JOIN pito_level lvl ON a.id_level_set = lvl.id_level_set
-            WHERE a.registration_id = %s AND a.is_transcribed = 1
+            WHERE a.registration_id = %s
             """
             conn = create_db_connection()
             cursor = conn.cursor()
